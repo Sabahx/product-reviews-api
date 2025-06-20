6d20bd588d1ecb2be7a14222a1b7a3ab3345a1ab
Ken <ahmadsoud63@gmail.com>
2025-06-20 18:31:06 +0300

    update review urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,11 @@
 
 from rest_framework.routers import DefaultRouter
-from .views import ReviewViewSet,RegisterView
+from .views import ReviewViewSet,RegisterView, ProductViewSet
 from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'reviews', ReviewViewSet, basename='review')
+router.register(r'products', ProductViewSet, basename='product') 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
