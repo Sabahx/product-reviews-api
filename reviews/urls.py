0d4e224f4b88d804c64f252921113f09f81b3a50
Ken <ahmadsoud63@gmail.com>
2025-06-20 23:34:56 +0300

    update view

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,11 +1,13 @@
 
 from rest_framework.routers import DefaultRouter
-from .views import ReviewViewSet,RegisterView, ProductViewSet
+from .views import ReviewCommentViewSet, ReviewViewSet,RegisterView, ProductViewSet, ReviewVoteViewSet
 from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'reviews', ReviewViewSet, basename='review')
-router.register(r'products', ProductViewSet, basename='product') 
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'review-comments', ReviewCommentViewSet, basename='review-comment')
+router.register(r'review-votes', ReviewVoteViewSet, basename='review-vote') 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
--- reviews/views.py
+++ reviews/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, ReviewCommentSerializer, ReviewVoteSerializer
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Review
+from .models import Review, ReviewComment, ReviewVote
 from .serializers import ReviewSerializer
 from .permissions import IsOwnerOrReadOnly
 from rest_framework import generics
@@ -32,7 +32,43 @@ class ReviewViewSet(viewsets.ModelViewSet):
         review = self.get_object()
         review.visible = True
         review.save()
-        return Response({'status': 'تمت الموافقة على المراجعة'})
+        return Response({'status': 'Review Approved'})
+    
+    @action(detail=True, methods=['get', 'post'])
+    def comments(self, request, pk=None):
+        review = self.get_object()
+        if request.method == 'GET':
+            comments = review.comments.all()
+            serializer = ReviewCommentSerializer(comments, many=True)
+            return Response(serializer.data)
+        elif request.method == 'POST':
+            serializer = ReviewCommentSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(review=review, user=request.user)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'])
+    def vote(self, request, pk=None):
+        review = self.get_object()
+        user = request.user
+        helpful = request.data.get('helpful', None)
+        
+        if helpful is None:
+            return Response({'error': 'Helpful field is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        vote, created = ReviewVote.objects.get_or_create(
+            review=review,
+            user=user,
+            defaults={'helpful': helpful}
+        )
+        
+        if not created:
+            vote.helpful = helpful
+            vote.save()
+        
+        serializer = ReviewVoteSerializer(vote)
+        return Response(serializer.data)
 
 
 class RegisterView(generics.CreateAPIView):
@@ -57,3 +93,19 @@ class ProductViewSet(viewsets.ModelViewSet):
         reviews = product.reviews.filter(visible=True)
         serializer = ReviewSerializer(reviews, many=True, context={'request': request})
         return Response(serializer.data)
+
+class ReviewCommentViewSet(viewsets.ModelViewSet):
+    queryset = ReviewComment.objects.all()
+    serializer_class = ReviewCommentSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ReviewVoteViewSet(viewsets.ModelViewSet):
+    queryset = ReviewVote.objects.all()
+    serializer_class = ReviewVoteSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
